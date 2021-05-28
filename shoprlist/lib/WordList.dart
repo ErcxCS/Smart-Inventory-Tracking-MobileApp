5f56365543b30c07827fde6942d85bc00190ad92
Berk Ercin <berkerciks@gmail.com>
2021-05-28 18:13:30 +0300

    json stuff

---
+++ shoprlist/lib/WordList.dart
@@ -0,0 +1,101 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair wordpair) {
+    final alreadySaved = _saved.contains(wordpair);
+
+    return ListTile(
+
+      title: Text(
+        wordpair.asPascalCase,
+        style: _biggerFont,
+      ),
+
+      trailing: Icon(
+        alreadySaved ? Icons.check_box_rounded : Icons.check_box_outline_blank_rounded,
+        color: alreadySaved ? Colors.red : Colors.yellow,
+      ),
+
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(wordpair);
+          }
+          else {
+            _saved.add(wordpair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+
+          final divided = tiles.isNotEmpty ? ListTile.divideTiles(context: context, tiles: tiles).toList() : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+
+        },
+      )
+    );
+  }
+}
